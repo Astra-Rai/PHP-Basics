8cab4f169383ea111d4783b03548ce119be10ea6
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2019-05-06 19:28:19 -0400

    Switch-Case

---
+++ 9-PHP-Swith-Case.php
@@ -0,0 +1,42 @@
+<?php
+$today;
+switch($today){
+	case: "Monday";
+	echo "Upper body weight training | 2-mile jog";
+	break; 
+	
+switch($today){
+	case: "Tuesday";
+	echo "Lower body weight training (quads) | 2-mile walk";
+	break; 
+}
+
+switch($today){
+	case:"Wednesday";
+	echo "Amino Acids supplement | 5 miles on bike";
+	break; 
+
+}
+
+switch($today){
+	case: "Thursday";
+	echo "Upper body workout | 2-mile jog";
+	break;
+}
+
+switch($today){
+	case: "Friday";
+	echo "Upper body workout | 2-mile jog";
+	break; 
+}
+
+switch($today){
+	case: "Saturday";
+	echo "Rest day | 1 Hour Stretching";
+	break; 
+}
+
+default: 
+	echo: "No information available for that day..."
+	break;
+}?>
